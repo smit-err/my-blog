cfec19b4e5dd70da8a4e3b36bdaf12039454922f
smit <smitmistry19@gmail.com>
2025-04-28 10:15:58 +0530

    added https

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "blog-site-atw.pages.dev",
+  site: "https://blog-site-atw.pages.dev",
   vite: {
     plugins: [tailwindcss()],
   },
