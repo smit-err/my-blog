5647320aa36210f04c893403348768a9803dc234
smit <smitmistry19@gmail.com>
2025-04-28 10:15:20 +0530

    added site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "blog-site-atw.pages.dev",
   vite: {
     plugins: [tailwindcss()],
   },
