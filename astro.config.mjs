1ba2d06598cb75ca9838261c20421e01d2d01455
smit <smitmistry19@gmail.com>
2025-04-28 10:03:04 +0530

    initial commit for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  devToolbar: { enabled: false },
+  markdown: {
+    shikiConfig: {
+      theme: "light-plus",
+    },
+  },
+});
